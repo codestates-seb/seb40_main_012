e4f442c8aa76a3859a7fd0fe5238c7d45bf28a83
gangdodan <dodannnn4@gmail.com>
2022-11-09 13:47:45 +0900

    first commit

---
+++ back/src/main/java/seb40_main_012/back/BackApplication.java
@@ -0,0 +1,13 @@
+package seb40_main_012.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+}
