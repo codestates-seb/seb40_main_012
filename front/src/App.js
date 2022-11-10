245ce1e4b5a237af6e9236d096d37bb0c6bfce45
journey <public.jinhui.kim@gmail.com>
2022-11-10 23:59:49 +0900

    [FE] fix: test 소스 제거

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,7 @@
-import { useState, useEffect } from 'react';
-import axios from './api/axios';
-
 const App = () => {
-  const [testData, setTestData] = useState('');
-
-  useEffect(() => {
-    axios.get('/').then((response) => {
-      return setTestData(response.data.testBody);
-    });
-  }, [testData]);
-
   return (
     <>
       <div>version: {process.env.REACT_APP_SERVICE_VERSION}</div>
-      <span>{testData}</span>
     </>
   );
 };
