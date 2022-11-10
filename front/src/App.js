a85463fa3265699c628619a40b3398463de2b8b0
journey <public.jinhui.kim@gmail.com>
2022-11-11 02:09:17 +0900

    [FE] feat: add Header

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
 import Header from './components/Header';
 import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
@@ -8,6 +9,7 @@ import NoMatch from './pages/NoMatch';
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
