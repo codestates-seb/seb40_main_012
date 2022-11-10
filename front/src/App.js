5d3bda5874d46970bb5e3e5a939e078a385cb665
journey <public.jinhui.kim@gmail.com>
2022-11-10 23:31:10 +0900

    [FE] feat: cors test

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
+import axios from './api/axios';
+
 const App = () => {
-  return <div>App</div>;
+  const [testData, setTestData] = useState('');
+
+  useEffect(() => {
+    axios.get('/').then((response) => {
+      return setTestData(response.data.testBody);
+    });
+  }, [testData]);
+
+  return (
+    <>
+      <div>version: {process.env.REACT_APP_SERVICE_VERSION}</div>
+      <span>{testData}</span>
+    </>
+  );
 };
 
 export default App;
---
+++ front/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export default instance;
