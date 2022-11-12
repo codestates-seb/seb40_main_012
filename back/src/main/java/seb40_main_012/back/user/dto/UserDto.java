973009a279cb60156f96acfccea10ca7ee57cdf4
hayoung10 <mhymsy@gmail.com>
2022-11-12 23:39:35 +0900

    [BE] feat: 회원가입 기능 추가

--- back/src/main/java/seb40_main_012/back/user/dto/UserDto.java
+++ back/src/main/java/seb40_main_012/back/user/dto/UserDto.java
@@ -1,9 +1,31 @@
 package seb40_main_012.back.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+//@Getter
 public class UserDto {
-    public static class PostDto{}
-    public static class ResponseDto{}
+    @Getter
+    @AllArgsConstructor
+    public static class PostDto{
+        @NotBlank
+        @Email
+        private String email;
+
+        @NotBlank
+        private String nickname;
+
+        @NotBlank
+        private String password;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ResponseDto{
+        private String email;
+        private String nickname;
+    }
 }
