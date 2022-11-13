6a857508edcb7955d77489b4783d5c58665e8a45
gnidinger <gcnb87@gmail.com>
2022-11-13 16:47:32 +0900

    [BE]Fix Typo

--- back/src/main/java/seb40_main_012/back/config/CorsConfig.java
+++ back/src/main/java/seb40_main_012/back/config/CorsConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-
 @Configuration
-public class corsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
