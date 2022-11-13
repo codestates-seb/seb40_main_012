c25e64181237defd3d277208d47372d9d8d260e1
jwo0o0 <jwo0o0358@gmail.com>
2022-11-13 20:58:21 +0900

    [FE] feat: Collection Page 추가

---
+++ front/src/pages/CollectionPage/Collection.js
@@ -0,0 +1,13 @@
+//import styled from 'styled-components';
+import PageContainer from '../../components/PageContainer';
+import CollectionHeader from './CollectionHeader';
+
+const CollectionPage = () => {
+  return (
+    <PageContainer footer>
+      <CollectionHeader />
+    </PageContainer>
+  );
+};
+
+export default CollectionPage;
