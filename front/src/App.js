fb14971e146eb3675c4630f7fe3ff4c3484bd452
serin-B <bangserin980302@gmail.com>
2022-11-16 13:24:22 +0900

    [FE] feat: pairing Write page 파일 생성

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,7 @@ import PairingFood from './pages/PairingPage/PairingCuisine';
 import PairingMusic from './pages/PairingPage/PairingMusic';
 import PairingBook from './pages/PairingPage/PairingBook';
 import PairingEtc from './pages/PairingPage/PairingEtc';
+import PairingWrite from './pages/PairingPage/PairingWrite/PairingWrite';
 import PairingDetail from './pages/PairingPage/PairingDetail/PairingDetail';
 import CollectionPage from './pages/CollectionPage/Collection';
 import CollectionDetailPage from './pages/CollectionDetailPage/CollectionDetail';
@@ -27,7 +28,8 @@ const App = () => {
         <Route path="/pairing/music" element={<PairingMusic />} />
         <Route path="/pairing/book" element={<PairingBook />} />
         <Route path="/pairing/etc" element={<PairingEtc />} />
-        <Route path="/pairing/:pairing_id" element={<PairingDetail />} />
+        <Route path="/pairing/write" element={<PairingWrite />} />
+        <Route path="/pairing/:pairingId" element={<PairingDetail />} />
         <Route path="/collection" element={<CollectionPage />} />
         <Route path="/user/signin" element={<SignInPage />} />
         <Route path="/user/signup" element={<SignUpPage />} />
