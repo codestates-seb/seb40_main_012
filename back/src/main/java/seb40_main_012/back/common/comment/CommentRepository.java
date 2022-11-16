e0df7c30b85cca99cee48c989317c176c3d1ac1a
gnidinger <gcnb87@gmail.com>
2022-11-16 20:35:34 +0900

    [BE]Fix - 책 장르 설정, 알라딘 API 적용, 검색기능 구현 시작

--- back/src/main/java/seb40_main_012/back/common/comment/CommentRepository.java
+++ back/src/main/java/seb40_main_012/back/common/comment/CommentRepository.java
@@ -3,22 +3,17 @@ package seb40_main_012.back.common.comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import seb40_main_012.back.common.comment.entity.Comment;
+import seb40_main_012.back.common.comment.entity.Comment;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Slice<Comment> findSliceBy(Pageable pageable);
 
-import seb40_main_012.back.common.comment.entity.Comment;
-
-import java.util.Optional;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
     Long countBy();
 
 }
