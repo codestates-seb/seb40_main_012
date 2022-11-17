45eb517f18bc7b4f48aae494427399bbaa11fe12
journey <public.jinhui.kim@gmail.com>
2022-11-17 17:09:45 +0900

    [FE] fix: api url 수정

--- front/src/api/requests.js
+++ front/src/api/requests.js
@@ -1,16 +1,21 @@
-export const SIGN_UP_URL = '/api/users';
+// 유저
+export const USERS_URL = '/api/users';
 export const SIGN_IN_URL = '/api/login';
 
-export const PAIRING_URL = '/api/books/pairings';
-export const PAIRING_ALL_LIKE_URL = '/api/books/pairings/likes';
-export const PAIRING_ALL_NEWEST_URL = '/api/books/pairings/newest';
-export const PAIRING_FILM_LIKE_URL = '/api/books/pairings/film/likes';
-export const PAIRING_FILM_NEWEST_URL = '/api/books/pairings/film/newest';
-export const PAIRING_CUISINE_LIKE_URL = '/api/books/pairings/cuisine/likes';
-export const PAIRING_CUISINE_NEWEST_URL = '/api/books/pairings/cuisine/newest';
-export const PAIRING_MUSIC_LIKE_URL = '/api/books/pairings/music/likes';
-export const PAIRING_MUSIC_NEWEST_URL = '/api/books/pairings/music/newest';
-export const PAIRING_BOOK_LIKE_URL = '/api/books/pairings/book/likes';
-export const PAIRING_BOOK_NEWEST_URL = '/api/books/pairings/book/newest';
-export const PAIRING_ETC_LIKE_URL = '/api/books/pairings/etc/likes';
-export const PAIRING_ETC_NEWEST_URL = '/api/books/pairings/etc/newest';
+// 책
+export const BOOKS_URL = '/api/books';
+
+// 페어링
+export const PAIRING_URL = `${BOOKS_URL}/pairings`;
+export const PAIRING_ALL_LIKE_URL = `${PAIRING_URL}/likes`;
+export const PAIRING_ALL_NEWEST_URL = `${PAIRING_URL}/newest`;
+export const PAIRING_FILM_LIKE_URL = `${PAIRING_URL}/film/likes`;
+export const PAIRING_FILM_NEWEST_URL = `${PAIRING_URL}/film/newest`;
+export const PAIRING_CUISINE_LIKE_URL = `${PAIRING_URL}/cuisine/likes`;
+export const PAIRING_CUISINE_NEWEST_URL = `${PAIRING_URL}/cuisine/newest`;
+export const PAIRING_MUSIC_LIKE_URL = `${PAIRING_URL}/music/likes`;
+export const PAIRING_MUSIC_NEWEST_URL = `${PAIRING_URL}/music/newest`;
+export const PAIRING_BOOK_LIKE_URL = `${PAIRING_URL}/book/likes`;
+export const PAIRING_BOOK_NEWEST_URL = `${PAIRING_URL}/book/newest`;
+export const PAIRING_ETC_LIKE_URL = `${PAIRING_URL}/etc/likes`;
+export const PAIRING_ETC_NEWEST_URL = `${PAIRING_URL}/etc/newest`;
