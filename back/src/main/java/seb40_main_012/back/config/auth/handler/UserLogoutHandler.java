0a56306190c9aec3341dfabe707a69fd198b74df
hayoung10 <mhymsy@gmail.com>
2022-11-22 16:55:53 +0900

    [BE] refactor: Security 리팩토링

--- back/src/main/java/seb40_main_012/back/config/auth/handler/UserLogoutHandler.java
+++ back/src/main/java/seb40_main_012/back/config/auth/handler/UserLogoutHandler.java
@@ -7,8 +7,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
+import seb40_main_012.back.config.auth.cookie.CookieManager;
 import seb40_main_012.back.config.auth.jwt.JwtTokenizer;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,17 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class UserLogoutHandler implements LogoutHandler {
     private final JwtTokenizer jwtTokenizer;
+    private final CookieManager cookieManager;
 
     @SneakyThrows
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response,
                        Authentication authentication) {
-        String refreshToken = jwtTokenizer.outCookie(request);
+        String refreshToken = cookieManager.outCookie(request, "refreshToken");
         jwtTokenizer.removeRefreshToken(refreshToken);
         try {
             jwtTokenizer.verifySignature(refreshToken);
         } catch (ExpiredJwtException ee) {
             response.sendError(401, "Refresh Token이 만료되었습니다");
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                cookies[i].setMaxAge(0); // 유효시간을 0으로 설정
+                response.addCookie(cookies[i]);
+            }
         }
 
         if(authentication != null)
