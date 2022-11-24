14c9f48b5a475366a72277168a42428f0baa35d3
hayoung10 <mhymsy@gmail.com>
2022-11-24 12:18:08 +0900

    [BE] feat: 사용자 선호 장르로 컬렉션 조회

--- back/src/main/java/seb40_main_012/back/book/BookRepository.java
+++ back/src/main/java/seb40_main_012/back/book/BookRepository.java
@@ -1,6 +1,7 @@
 package seb40_main_012.back.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     Optional<Book> findByIsbn13(String isbn13);
 
