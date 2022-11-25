5e50c90d539154a543a56ae270d3d2ac25ab5ece
gnidinger <gcnb87@gmail.com>
2022-11-25 10:29:39 +0900

    [BE]책 검색 로직 수정

--- back/src/main/java/seb40_main_012/back/email/EmailSenderService.java
+++ back/src/main/java/seb40_main_012/back/email/EmailSenderService.java
@@ -23,7 +23,7 @@ public class EmailSenderService {
                             "<br><br>" +
                             "<font size = \"5\">" +
                             "<h1>안녕하세요, 체리<font color=\"#6741FF\">픽</font> 팀입니다.</h1>" +
-                            "<h2>" + "&lt;달리는 감자&gt;에서 🍒를 담 만들었어요." + "</h2>" +
+                            "<h2>" + "&lt;달리는 감자&gt;에서 🍒를 담아 만들었어요." + "</h2>" +
                             "<h2>책과 이어지는 페어링으로 함께 온기를 나누어요.</h2>" +
                             "<h3>바로가기: <A href = \"http://localhost:8080/\" target = \"blank\">http://localhost:8080/</A></h3>" +
                             "<font>";
