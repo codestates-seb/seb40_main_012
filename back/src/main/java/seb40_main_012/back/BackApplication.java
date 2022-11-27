fdbb99af99a3a15a44ff41b1df55a17e0a773afc
gnidinger <gcnb87@gmail.com>
2022-11-27 14:07:19 +0900

    [BE]페어링 이미지 반영 속도 수정

--- back/src/main/java/seb40_main_012/back/BackApplication.java
+++ back/src/main/java/seb40_main_012/back/BackApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackApplication {
