f58809c54b31ffd218679fae8bb292aa5ba7c71a
journey <public.jinhui.kim@gmail.com>
2022-11-28 06:10:15 +0900

    [FE] feat: 헤더 반응형으로 수정

--- front/src/containers/Header.js
+++ front/src/containers/Header.js
@@ -1,99 +1,197 @@
-import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import styled from 'styled-components';
 import { Searchbar } from 'components';
 import { selectIsLogin, selectProfileImage } from 'store/modules/authSlice';
-import { authApi } from 'api';
 import { dummyUserImgUrl } from 'util/userAvatar';
+import { authApi } from 'api';
 
-const HeaderContainer = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
+const drawerWidth = 240;
+const PAIRING = '페어링';
+const COLLECTION = '컬렉션';
+const navItems = [PAIRING, COLLECTION];
+
+const HeaderContainerStyled = styled(AppBar)`
   height: 60px;
   background-color: white;
   border-bottom: 1px solid ${({ theme }) => theme.colors.lightgray};
+  /* z-index: 5050; */
+  box-shadow: none;
+`;
+
+const ToolbarStyled = styled(Toolbar)`
+  min-height: 60px;
+`;
+
+const LogoContainerStyled = styled.div`
+  margin: 0 40px 0 20px;
+`;
+
+const LogoSytled = styled.img`
+  padding: 17px 0 10px;
+`;
+
+const HeaderBtn = styled(Button)`
+  color: ${({ theme }) => theme.colors.dark};
+  border: none;
   &:hover {
-    cursor: pointer;
-  }
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  z-index: 5050;
-  div.header_left {
-    display: flex;
-    align-items: center;
-  }
-  div.header_right {
-    display: flex;
-    align-items: center;
+    background-color: transparent;
   }
 `;
 
-const LogoContainer = styled.div`
-  margin: 0 40px;
-  margin-top: 7px;
-`;
-
-const HeaderBtn = styled.button`
-  background-color: transparent;
+const HeaderItemButtonStyled = styled(HeaderBtn)`
   font-size: 16px;
   font-weight: 700;
-  border: none;
-  border-bottom: 3px solid transparent;
   margin: 0 15px;
-  width: 60px;
+  border-bottom: 3px solid transparent;
+  height: 60px;
+  border-top: 3px solid transparent;
+  &:hover,
+  &.selected {
+    border-bottom: 3px solid ${({ theme }) => theme.colors.mainColor};
+  }
+`;
+
+const DrawerListItemButtonStyled = styled(ListItemButton)`
+  &.selected {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
   &:hover {
-    cursor: pointer;
+    background-color: ${({ theme }) => theme.colors.purple_3};
   }
 `;
 
-const PairingBtn = styled(HeaderBtn)`
-  height: 60px;
-  border-top: 3px solid transparent;
-  &:hover,
-  &.selected {
-    border-bottom: 3px solid ${({ theme }) => theme.colors.mainColor};
-  }
-`;
-
-const CollectionBtn = styled(HeaderBtn)`
-  height: 60px;
-  border-top: 3px solid transparent;
-  &:hover,
-  &.selected {
-    border-bottom: 3px solid ${({ theme }) => theme.colors.mainColor};
-  }
-`;
-
-const LoginOutBtn = styled(HeaderBtn)`
+const AuthButtounStyled = styled(HeaderBtn)`
   font-size: 13px;
   font-weight: 400;
   &:hover {
-    cursor: pointer;
     color: ${({ theme }) => theme.colors.mainColor};
   }
-  margin-left: 10px;
 `;
 
-const MyPageIconContainer = styled.div`
+const MyPageIconContainer = styled(Link)`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-right: 40px;
-  &:hover {
-    cursor: pointer;
+  margin: 0 20px 0 10px;
+  @media screen and (max-width: 600px) {
+    margin-right: 0px;
   }
 `;
 
-const Header = () => {
+const logo = (
+  <Link to="/">
+    <LogoSytled
+      src={process.env.PUBLIC_URL + '/images/CherryPick.svg'}
+      alt="CherryPick main logo"
+    />
+  </Link>
+);
+
+function Header(props) {
+  const navigate = useNavigate();
   const location = useLocation();
   const isLogin = useSelector(selectIsLogin);
   const profileImage = useSelector(selectProfileImage);
 
-  const navigate = useNavigate();
+  const userAvatar = (
+    <Avatar
+      alt="Mypage Icon"
+      src={profileImage ? profileImage : dummyUserImgUrl}
+      sx={{ width: 32, height: 32 }}
+    />
+  );
+
+  const loginUserItems = [
+    { text: '마이페이지', icon: userAvatar },
+    { text: '로그아웃', icon: <LogoutIcon /> },
+  ];
+  const nonMembersItems = [
+    { text: '로그인', icon: <LoginIcon /> },
+    { text: '회원가입', icon: <AccountCircle /> },
+  ];
+
+  const { window } = props;
+  const [drawerOpen, setDrawerOpen] = useState({
+    left: false,
+    right: false,
+  });
+  const [selectedIndex, setSelectedIndex] = useState({
+    left: null,
+    right: null,
+  });
+
+  useEffect(() => {
+    const { pathname } = location;
+    if (pathname.startsWith('/pairing')) {
+      setSelectedIndex({ ...selectedIndex, left: navItems.indexOf(PAIRING) });
+      return;
+    }
+    if (pathname.startsWith('/collection')) {
+      setSelectedIndex({
+        ...selectedIndex,
+        left: navItems.indexOf(COLLECTION),
+      });
+      return;
+    }
+  }, [location]);
+
+  const handleDrawerToggle = (anchor) => {
+    setDrawerOpen((prev) => {
+      return { ...prev, [anchor]: !prev[anchor] };
+    });
+  };
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  const handleClickDrawerListItem = (e) => {
+    switch (e.target.textContent) {
+      case PAIRING:
+        navigate('/pairing');
+        break;
+      case COLLECTION:
+        navigate('/collection');
+        break;
+      case '로그인':
+        navigate('/user/signin');
+        break;
+      case '회원가입':
+        navigate('/user/signup');
+        break;
+      case '마이페이지':
+        navigate('/mypage');
+        break;
+      case '로그아웃':
+        handleClickLogoutButton();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClickNavigateButton = (url) => {
+    navigate(url);
+  };
 
   const handleClickLogoutButton = async () => {
     try {
@@ -103,72 +201,190 @@ const Header = () => {
     }
   };
 
-  const handleClickAvatar = () => {
-    if (isLogin) {
-      navigate('/mypage');
-      return;
-    }
-    navigate('/user/signup');
-  };
-
   return (
-    <HeaderContainer>
-      <div className="header_left">
-        <LogoContainer>
-          <Link to="/">
-            <img
-              src={process.env.PUBLIC_URL + '/images/CherryPick.svg'}
-              alt="CherryPick main logo"
-            />
-          </Link>
-        </LogoContainer>
-        <Link to="/pairing">
-          <PairingBtn
-            className={location.pathname === '/pairing' ? 'selected' : null}
+    <Box sx={{ display: 'flex' }}>
+      <HeaderContainerStyled component="nav">
+        <ToolbarStyled>
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={() => handleDrawerToggle('left')}
+            sx={{ mr: 2, display: { md: 'none' }, flex: '0 0 0' }}
           >
-            페어링
-          </PairingBtn>
-        </Link>
-        <Link to="/collection">
-          <CollectionBtn
-            className={location.pathname === '/collection' ? 'selected' : null}
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              display: {
+                xs: 'none',
+                sm: 'flex',
+              },
+              flexGrow: 1,
+            }}
           >
-            컬렉션
-          </CollectionBtn>
-        </Link>
-      </div>
-      <div className="header_right">
-        <Searchbar />
-        {isLogin ? (
-          <>
-            <LoginOutBtn onClick={handleClickLogoutButton}>
-              로그아웃
-            </LoginOutBtn>
-            <MyPageIconContainer onClick={handleClickAvatar}>
-              <Avatar
-                alt="Mypage Icon"
-                src={profileImage ? profileImage : dummyUserImgUrl}
-                sx={{ width: 32, height: 32 }}
-              />
-            </MyPageIconContainer>
-          </>
-        ) : (
-          <>
-            <Link to="/user/signin">
-              <LoginOutBtn>로그인</LoginOutBtn>
-            </Link>
-            {/* <MyPageIconContainer onClick={handleClickAvatar}>
-              <Avatar
-                alt="Sign Up Icon"
-                src={dummyUserImgUrl}
-                sx={{ width: 32, height: 32 }}
-              />
-            </MyPageIconContainer> */}
-          </>
-        )}
-      </div>
-    </HeaderContainer>
+            <LogoContainerStyled>{logo}</LogoContainerStyled>
+            {navItems.map((item, index) => (
+              <HeaderItemButtonStyled
+                sx={{ display: { sm: 'none', md: 'flex' } }}
+                className={selectedIndex.left === index ? 'selected' : ''}
+                key={item}
+                onClick={handleClickDrawerListItem}
+              >
+                {item}
+              </HeaderItemButtonStyled>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flex: { xs: '1 0 auto', sm: '0 1 0' },
+            }}
+          >
+            <Searchbar />
+            <Box
+              sx={{
+                display: { xs: 'none', md: 'flex' },
+                alignItems: 'center',
+              }}
+            >
+              {isLogin ? (
+                <>
+                  <AuthButtounStyled onClick={handleClickLogoutButton}>
+                    로그아웃
+                  </AuthButtounStyled>
+                  <MyPageIconContainer to="/mypage">
+                    {userAvatar}
+                  </MyPageIconContainer>
+                </>
+              ) : (
+                <>
+                  <AuthButtounStyled
+                    onClick={() => handleClickNavigateButton('/user/signin')}
+                  >
+                    로그인
+                  </AuthButtounStyled>
+                  <AuthButtounStyled
+                    onClick={() => handleClickNavigateButton('/user/signup')}
+                  >
+                    회원가입
+                  </AuthButtounStyled>
+                </>
+              )}
+            </Box>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              onClick={() => {
+                handleDrawerToggle('right');
+              }}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+                marginLeft: '16px',
+                marginRight: '-12px',
+                padding: '8px',
+                flex: '0 0 0',
+              }}
+            >
+              {isLogin ? userAvatar : <MoreIcon />}
+            </IconButton>
+          </Box>
+        </ToolbarStyled>
+      </HeaderContainerStyled>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={drawerOpen.left}
+          onClose={() => handleDrawerToggle('left')}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box
+            onClick={() => handleDrawerToggle('left')}
+            sx={{ textAlign: 'center' }}
+          >
+            {logo}
+            <Divider />
+            <List>
+              {navItems.map((item, index) => (
+                <ListItem key={item} disablePadding>
+                  <DrawerListItemButtonStyled
+                    sx={{ textAlign: 'center' }}
+                    onClick={handleClickDrawerListItem}
+                    className={selectedIndex.left === index ? 'selected' : ''}
+                  >
+                    <ListItemText primary={item} />
+                  </DrawerListItemButtonStyled>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor="right"
+          open={drawerOpen.right}
+          onClose={() => handleDrawerToggle('right')}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box
+            onClick={() => handleDrawerToggle('right')}
+            sx={{ textAlign: 'center' }}
+          >
+            <List>
+              {isLogin
+                ? loginUserItems.map(({ text, icon }, index) => (
+                    <ListItem key={text} disablePadding>
+                      <DrawerListItemButtonStyled
+                        onClick={handleClickDrawerListItem}
+                        className={
+                          selectedIndex.right === index ? 'selected' : ''
+                        }
+                      >
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={text} />
+                      </DrawerListItemButtonStyled>
+                    </ListItem>
+                  ))
+                : nonMembersItems.map(({ text, icon }, index) => (
+                    <ListItem key={text} disablePadding>
+                      <DrawerListItemButtonStyled
+                        onClick={handleClickDrawerListItem}
+                        className={
+                          selectedIndex.right === index ? 'selected' : ''
+                        }
+                      >
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={text} />
+                      </DrawerListItemButtonStyled>
+                    </ListItem>
+                  ))}
+            </List>
+          </Box>
+        </Drawer>
+      </Box>
+    </Box>
   );
-};
+}
 
 export default Header;
