d6af9d2793d5190ba1bb80ca926ddf4b44c97e66
jwo0o0 <jwo0o0358@gmail.com>
2022-11-28 10:55:54 +0900

    [FE] feat: 검색페이지 Tabbar 구현

--- front/src/pages/SearchPage/SearchBookPage.js
+++ front/src/pages/SearchPage/SearchBookPage.js
@@ -1,7 +1,12 @@
 import { PageContainer } from 'containers';
+import SearchTab from './SearchTab';
 
 const SearchBookPage = () => {
-  return <PageContainer footer></PageContainer>;
+  return (
+    <PageContainer footer>
+      <SearchTab />
+    </PageContainer>
+  );
 };
 
 export default SearchBookPage;
