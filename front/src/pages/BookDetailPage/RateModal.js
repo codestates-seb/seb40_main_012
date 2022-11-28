0d8cc85b45f1395f1d80401ce3af7cc2fe80939f
serin-B <bangserin980302@gmail.com>
2022-11-28 16:08:57 +0900

    [FE] feat: 책 상세페이지 코멘트 및 별점 기능 구현

--- front/src/pages/BookDetailPage/RateModal.js
+++ front/src/pages/BookDetailPage/RateModal.js
@@ -1,26 +1,71 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { FillButton } from '../../components/Buttons';
+import TextField from '@mui/material/TextField';
+import styled from 'styled-components';
+import { FillButton, ContainedButton } from '../../components/Buttons';
 import HoverRating from './RateStar';
-import CommentAdd from '../../components/Comments/CommentAdd';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  postBookComment,
+  patchBookStarRating,
+} from '../../store/modules/bookSlice';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 900,
+  width: 400,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
 };
 
-export default function RateModal() {
-  const [open, setOpen] = React.useState(false);
+const DivStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 0px 5px 70px;
+`;
+
+const LimitDesc = styled.p`
+  color: gray;
+  font-size: 13px;
+  margin-left: 15px;
+`;
+
+export default function RateModal({ isbn }) {
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [star, setStar] = useState(0);
+  const [comment, setComment] = useState('');
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  console.log('별점', star);
+  console.log('코멘트', comment);
+
+  const handleSubmit = () => {
+    if (comment.length > 4) {
+      const commentBody = { body: comment };
+      dispatch(postBookComment({ isbn, commentBody }));
+    }
+    if (star > 0) {
+      const ratingBody = { rating: star };
+      dispatch(patchBookStarRating({ isbn, ratingBody }));
+    }
+    setStar(0);
+    setComment('');
+    handleClose();
+  };
 
   return (
     <>
@@ -35,8 +80,33 @@ export default function RateModal() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             이 책에 대한 별점과 코멘트를 달아보세요
           </Typography>
-          <HoverRating />
-          <CommentAdd />
+          <DivStyled>
+            <HoverRating star={star} setStar={setStar} />
+          </DivStyled>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '98%' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="outlined-multiline-static"
+              placeholder="코멘트를 입력해주세요"
+              value={comment}
+              onChange={handleComment}
+              multiline
+              rows={3}
+            />
+          </Box>
+          {comment.length < 5 ? (
+            <LimitDesc>5글자 이상 입력하세요.</LimitDesc>
+          ) : (
+            <ContainedButton width="100%" onClick={handleSubmit}>
+              등록
+            </ContainedButton>
+          )}
         </Box>
       </Modal>
     </>
