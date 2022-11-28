957e62793bf504519c7d3498f0a7d2a933050c9b
serin-B <bangserin980302@gmail.com>
2022-11-28 16:07:32 +0900

    [FE] feat: 페어링 랜덤 큐레이션 추가

--- front/src/store/modules/allPairingSlice.js
+++ front/src/store/modules/allPairingSlice.js
@@ -3,11 +3,13 @@ import axios from '../../api/axios';
 import {
   PAIRING_ALL_LIKE_URL,
   PAIRING_ALL_NEWEST_URL,
+  PAIRING_ALL_RANDOM_URL,
 } from '../../api/requests';
 
 const initialState = {
   likeData: [],
   newestData: [],
+  randomData: [],
   status: '',
 };
 
@@ -35,6 +37,18 @@ export const asyncGetAllPairingNewest = createAsyncThunk(
   }
 );
 
+export const asyncGetAllPairingRandom = createAsyncThunk(
+  'pairingSlice/asyncGetAllPairingRandom',
+  async () => {
+    try {
+      const res = await axios.get(PAIRING_ALL_RANDOM_URL);
+      return res.data.data.content;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export const allPairingSlice = createSlice({
   name: 'getPairing',
   initialState,
@@ -44,6 +58,7 @@ export const allPairingSlice = createSlice({
       state.status = 'pending';
     });
     builder.addCase(asyncGetAllPairingLike.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
       state.likeData = action.payload;
     });
     builder.addCase(asyncGetAllPairingLike.rejected, (state) => {
@@ -53,9 +68,20 @@ export const allPairingSlice = createSlice({
       state.status = 'pending';
     });
     builder.addCase(asyncGetAllPairingNewest.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
       state.newestData = action.payload;
     });
     builder.addCase(asyncGetAllPairingNewest.rejected, (state) => {
+      state.status = 'rejected';
+    });
+    builder.addCase(asyncGetAllPairingRandom.pending, (state) => {
+      state.status = 'pending';
+    });
+    builder.addCase(asyncGetAllPairingRandom.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
+      state.randomData = action.payload;
+    });
+    builder.addCase(asyncGetAllPairingRandom.rejected, (state) => {
       state.status = 'rejected';
     });
   },
--- front/src/store/modules/musicPairingSlice.js
+++ front/src/store/modules/musicPairingSlice.js
@@ -3,11 +3,13 @@ import axios from '../../api/axios';
 import {
   PAIRING_MUSIC_LIKE_URL,
   PAIRING_MUSIC_NEWEST_URL,
+  PAIRING_MUSIC_RANDOM_URL,
 } from '../../api/requests';
 
 const initialState = {
   likeData: [],
   newestData: [],
+  randomData: [],
   status: '',
 };
 
@@ -35,6 +37,18 @@ export const asyncGetMusicPairingNewest = createAsyncThunk(
   }
 );
 
+export const asyncGetMusicPairingRandom = createAsyncThunk(
+  'pairingSlice/asyncGetMusicPairingRandom',
+  async () => {
+    try {
+      const res = await axios.get(PAIRING_MUSIC_RANDOM_URL);
+      return res.data.data.content;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export const musicPairingSlice = createSlice({
   name: 'getPairing',
   initialState,
@@ -44,6 +58,7 @@ export const musicPairingSlice = createSlice({
       state.status = 'pending';
     });
     builder.addCase(asyncGetMusicPairingLike.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
       state.likeData = action.payload;
     });
     builder.addCase(asyncGetMusicPairingLike.rejected, (state) => {
@@ -53,9 +68,20 @@ export const musicPairingSlice = createSlice({
       state.status = 'pending';
     });
     builder.addCase(asyncGetMusicPairingNewest.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
       state.newestData = action.payload;
     });
     builder.addCase(asyncGetMusicPairingNewest.rejected, (state) => {
+      state.status = 'rejected';
+    });
+    builder.addCase(asyncGetMusicPairingRandom.pending, (state) => {
+      state.status = 'pending';
+    });
+    builder.addCase(asyncGetMusicPairingRandom.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
+      state.randomData = action.payload;
+    });
+    builder.addCase(asyncGetMusicPairingRandom.rejected, (state) => {
       state.status = 'rejected';
     });
   },
