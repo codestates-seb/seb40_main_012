ca9d53f98821af685c7cddf6456bb3910399aac5
journey <public.jinhui.kim@gmail.com>
2022-11-29 11:29:30 +0900

    [FE] feat: LoadingComponent 추가

---
+++ front/src/components/LoadingComponent.js
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+};
+
+const LoadingComponent = () => {
+  return (
+    <Box sx={style}>
+      <img src={process.env.PUBLIC_URL + '/images/spinner.gif'} alt="loading" />
+    </Box>
+  );
+};
+
+export default LoadingComponent;
