a27df5e1312c84e968a64b9026b4dc5f1f137270
journey <public.jinhui.kim@gmail.com>
2022-12-02 01:28:14 +0900

    [FE] fix: axios interceptor로 리프레시 토큰 갱신

--- front/src/api/authApi.js
+++ front/src/api/authApi.js
@@ -43,25 +43,19 @@ const authApi = {
         })
         .catch((error) => {
           return reject(error);
-
-          // 에러코드 나오면 처리 필요
-          // logoutApi();
         });
     });
   },
   refreshToken: () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       return axios
         .get(TOKEN_REFRESH_URL)
         .then((response) => {
           signInSuccess(response);
           return resolve();
         })
-        .catch((error) => {
-          return reject(error);
-
-          // 에러코드 나오면 처리 필요
-          // logoutApi();
+        .catch(() => {
+          authApi.logout();
         });
     });
   },
