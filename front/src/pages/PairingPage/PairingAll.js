342eaccea5e8efa435a99574bdecd3475c427fdf
serin-B <bangserin980302@gmail.com>
2022-12-03 09:47:45 +0900

    [FE] feat: 페어링 체리픽 추가

--- front/src/pages/PairingPage/PairingAll.js
+++ front/src/pages/PairingPage/PairingAll.js
@@ -26,7 +26,16 @@ const PairingPage = () => {
   return (
     <PageContainer footer>
       <PairingTab />
-      <PairingCherryPick />
+      <PairingCherryPick
+        img="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FE2Let%2FbtrSHvnEktD%2FW1gIdYDKXQ2N57w75pQgDk%2Fimg.jpg"
+        pairingId="4"
+      >
+        <div>
+          세상을 홀로 견디는 기분.
+          <br />
+          우주도, 지구도, 서울도 똑같은 마음으로 여행중인 당신을 위해.
+        </div>
+      </PairingCherryPick>
       <PairingCuration title={titleLike} pairingData={pairingLikeData} />
       <PairingCuration title={titleNewest} pairingData={pairingNewestData} />
       <PairingCuration title={titleRandom} pairingData={pairingRandomData} />
