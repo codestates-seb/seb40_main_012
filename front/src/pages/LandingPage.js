5806b013e99598719b414068d55e963daf0b1e8f
serin-B <bangserin980302@gmail.com>
2022-12-06 01:06:38 +0900

    [FE] feat: 헤더에 책 추가, 로그인 여부에 따라 랜딩페이지 라우터 처리

---
+++ front/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import { PageContainer } from 'containers';
+
+const LandingPage = () => {
+  return (
+    <PageContainer footer center maxWidth="sm" bmt={5}>
+      <h1>랜딩페이지</h1>
+    </PageContainer>
+  );
+};
+
+export default LandingPage;
