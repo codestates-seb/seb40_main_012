49aca9dd7f7626b9d6559c7c7f3cadd8f792d1d7
gangdodan <dodannnn4@gmail.com>
2022-12-05 17:57:01 +0900

    Fix: 컬렉션 수정 > 컬렉션 북 eager 로딩 제거

--- back/src/main/java/seb40_main_012/back/bookCollectionBook/BookCollectionBook.java
+++ back/src/main/java/seb40_main_012/back/bookCollectionBook/BookCollectionBook.java
@@ -23,13 +23,13 @@ public class BookCollectionBook {
     @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
-    @LazyCollection(LazyCollectionOption.FALSE)
+//    @LazyCollection(LazyCollectionOption.FALSE)
     private Book book;
 
     @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "collection_id")
-    @LazyCollection(LazyCollectionOption.FALSE)
+//    @LazyCollection(LazyCollectionOption.FALSE)
     private BookCollection bookCollection;
 
     public BookCollectionBook(Book book,BookCollection collection){
